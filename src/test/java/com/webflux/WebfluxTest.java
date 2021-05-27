baf1fe9f6c86eeeb70090fe548b1a96b27db95a0
wally <wally@within.co.kr>
2021-05-27 18:10:52 +0900

    asdfsdfa

--- src/test/java/com/webflux/WebfluxTest.java
+++ src/test/java/com/webflux/WebfluxTest.java
@@ -10,25 +10,37 @@ import java.time.Duration;
 
 public class WebfluxTest {
 
+//    @Test
+//    public void websocketClient() throws InterruptedException {
+//
+//        ReactorNettyWebSocketClient client = new ReactorNettyWebSocketClient();
+//        client.execute(
+//                URI.create("http://localhost:8080/ws/echo"),
+//                session -> {
+//                    session.receive()
+//                            .subscribe(webSocketMessage ->
+//                                    System.out.println(webSocketMessage.getPayloadAsText()));
+//
+//                    return Flux
+//                            .interval(Duration.ofMillis(1000))
+//                            .map(String::valueOf)
+//                            .map(session::textMessage)
+//                            .as(session::send);
+//                }
+//        ).subscribe();
+//
+//        Thread.sleep(99999);
+//    }
+//
     @Test
-    public void websocketClient() throws InterruptedException {
-        ReactorNettyWebSocketClient client = new ReactorNettyWebSocketClient();
-        client.execute(
-                URI.create("http://localhost:8080/ws/echo"),
-                session -> {
+  public void test(){
 
-                    session.receive()
-                            .subscribe(webSocketMessage ->
-                                    System.out.println(webSocketMessage.getPayloadAsText()));
 
-                    return Flux
-                            .interval(Duration.ofMillis(1000))
-                            .map(String::valueOf)
-                            .map(session::textMessage)
-                            .as(session::send);
-                }
-        ).subscribe();
+      String a = "";
+      String b = "";
 
-        Thread.sleep(99999);
+      if(a.equals(b)) {
+        return;
+      }
     }
 }
